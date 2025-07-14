3d71c49dae3416e49545662f0967ae933e81824d
rishita <151740504+rishita211@users.noreply.github.com>
2025-07-14 07:46:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// When the user scrolls, add or remove the 'scrolled' class
+window.addEventListener('scroll', () => {
+  const navbar = document.querySelector('.navbar');
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
